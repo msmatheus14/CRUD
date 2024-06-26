63a05d316fc8919940a58e3f10aa767edb8e9f7d
MatheusAndrade <52432816+msmatheus14@users.noreply.github.com>
2024-06-26 01:41:53 +0000

    PrimeiroCommit

--- ORM.js
+++ ORM.js
@@ -1,10 +1,9 @@
 class ORM {
   constructor() {
-    if(new.target === ORM){
-      throw new Error("Classe Abstrata meu parceiro!")
+    if (new.target === ORM) {
+      throw new Error("Classe Abstrata meu parceiro!");
     }
-
-      }
+  }
 
   #buscarElemento(id) {
     let val = null;
@@ -23,57 +22,47 @@ class ORM {
   }
 
   create(id, obj) {
-    
-    if(typeof(id) !== "number"){
-      throw new Error('ID deve ser um número')
+    if (typeof id !== "number") {
+      throw new Error("ID deve ser um número");
     }
-    if(typeof(obj) !== "object"){
-      throw new Error ( 'obj deve ser um objeto')
+    if (typeof obj !== "object") {
+      throw new Error("obj deve ser um objeto");
     }
 
-    if(this.vetor_elemento.length == 0){
-      let obj2 = {id: id , ...obj}
+    if (this.vetor_elemento.length == 0) {
+      let obj2 = { id: id, ...obj };
 
-      console.log(`Adicionado ${id} com sucesso!`)
+      console.log(`Adicionado ${id} com sucesso!`);
 
       return this.vetor_elemento.push(obj2);
-    }else
-      
-    if(this.vetor_elemento.length > 0){
-      
-      let verificao = false
+    } else if (this.vetor_elemento.length > 0) {
+      let verificao = false;
 
-  
       this.vetor_elemento.map((n) => {
-        if(n.id == id){
-          verificao = true
+        if (n.id == id) {
+          verificao = true;
         }
-      })
+      });
 
-      if(verificao == true){
-        console.log(`Atenção. Erro! ID ${id} Ja existe no sistema.`)
-      }else
-      {
-        let obj2 = {id: id , ...obj}
-        console.log(`Adicionado ${id} com sucesso!`)
+      if (verificao == true) {
+        console.log(`Atenção. Erro! ID ${id} Ja existe no sistema.`);
+      } else {
+        let obj2 = { id: id, ...obj };
+        console.log(`Adicionado ${id} com sucesso!`);
         return this.vetor_elemento.push(obj2);
       }
-      
     }
-
   }
 
   update(id, obj_novo) {
     // Gabriel dessa forma ele só atualiza os atributos que o usuario especificar.
-    let elemento = this.#buscarElemento(id)
-    
+    let elemento = this.#buscarElemento(id);
+
     let obj = this.vetor_elemento[elemento];
-    
+
     obj = { ...obj, ...obj_novo };
 
     this.vetor_elemento[elemento] = obj;
-  
-
   }
 
   delete(id) {
@@ -83,8 +72,7 @@ class ORM {
 
   get read() {
     console.log(this.vetor_elemento);
-
-}
+  }
 }
 
 export default ORM;
--- index.js
+++ index.js
@@ -2,12 +2,8 @@ import USUARIO from "./Usuario.js";
 import Generico from "./Generico.js";
 
 let user = new USUARIO();
-user.create(1, {nome: "João", idade: 30})
-user.create(1, {nome: "Maria", idade: 25})
+user.create(1, { nome: "João", idade: 30 });
+user.create(1, { nome: "Maria", idade: 25 });
 
-user.create(3, {nome: "Pedro", idade: 40})
-user.create(4, {nome: "Ana", idade: 35})
-
-
-
-
+user.create(3, { nome: "Pedro", idade: 40 });
+user.create(4, { nome: "Ana", idade: 35 });
